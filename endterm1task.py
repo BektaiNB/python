2015124c0083456b1497b0239187031aca2ed9e9
BektaiNB <124173026+BektaiNB@users.noreply.github.com>
2023-12-09 13:57:28 +0600

    Add files via upload

---
+++ endterm1task.py
@@ -0,0 +1,27 @@
+
+course1_score = int(input("MATH:"))
+course2_score = int(input("CHEMISTRY:"))
+course3_score = int(input("ENGLISH:"))
+if course1_score & course2_score & course3_score == 95 or 96 or 97 or 98 or 99 or 100:
+    print(course1_score & course2_score & course3_score == 4.3) 
+elif course1_score & course2_score & course3_score == 94 or 93 or 92 or 91 or 90:
+    print(ccourse1_score & course2_score & course3_score) == 4.0
+elif course1_score & course2_score & course3_score == 89 or 88 or 87 or 86 or 85:
+    print(ccourse1_score & course2_score & course3_score) == 3.7
+elif course1_score & course2_score & course3_score == 84 or 83 or 82 or 81 or 80:
+    print(ccourse1_score & course2_score & course3_score) == 3.3
+elif course1_score & course2_score & course3_score == 79 or 78 or 77 or 76 or 75:
+    print(ccourse1_score & course2_score & course3_score) == 3.0
+elif course1_score & course2_score & course3_score == 74 or 73 or 72 or 71 or 70:
+    print(ccourse1_score & course2_score & course3_score) == 2.7
+elif course1_score & course2_score & course3_score == 69 or 68 or 67 or 66 or 65:
+    print(ccourse1_score & course2_score & course3_score) == 2.3
+elif course1_score & course2_score & course3_score == 64 or 63 or 62 or 61 or 60:
+    print(ccourse1_score & course2_score & course3_score) == 2.0
+elif course1_score & course2_score & course3_score == 59 or 58 or 57 or 56 or 55:
+    print(ccourse1_score & course2_score & course3_score) == 1.7
+elif course1_score & course2_score & course3_score > 54:
+    print(ccourse1_score & course2_score & course3_score) == 1.0
+
+
+
